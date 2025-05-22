739c49a31db3b9417f26d88036d7aeef8e2540fa
Bala <malharikingdom9@gmail.com>
2025-05-22 23:09:10 +0530

    Second Push

--- src/Components/AdminLogin.jsx
+++ src/Components/AdminLogin.jsx
@@ -8,6 +8,7 @@ export default function AdminLogin({
             <Paper elevation={6} className="glass-card" sx={{ p:4, minWidth: 350, maxWidth: 400}}>
                 <Typography variant="h5" mb={2} fontWeight={600}>Admin Login</Typography>
                 <form onSubmit={handleLogin} autoComplete="off">
+                    <Typography variant="caption" color="success">Email: "admin@example.com", Password: "admin123"</Typography>
                     <TextField label="Email" name="email" value={login.email} onChange={handleLoginChange} fullWidth margin="normal"/>
                     <TextField label="Password" name="password" type="password" value={login.password} onChange={handleLoginChange} fullWidth margin="normal"/>
                     {loginErr && <Typography variant="caption" color="error">{loginErr}</Typography>}
